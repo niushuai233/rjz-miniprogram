7a5267a88067e900faae7d4d5a23425a0c33e6cf
diguaganer <niushuai951101@gmail.com>
2020-08-13 17:19:09 +0800

    引入vuecookie

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
+import VueCookies from 'vue-cookies'
 import App from './App'
 import store from './store/index'
 import fly from './utils/fly'
 
+Vue.use(VueCookies)
 Vue.config.productionTip = false
 App.mpType = 'app'
 Vue.prototype.$fly = fly;
@@ -16,24 +18,24 @@ Vue.prototype.globalData = getApp().globalData
 
 // icon图标
 Vue.prototype.globalData.iconArray = {
-    payIcon: ["icon-yundong",
-            "icon-youxi",
-            "icon-yundong1",
-            "icon-lvyou",
-            "icon-gouwu",
-            "icon-jiaotong",
-            "icon-shenghuo",
-            "icon-xuexi-",
-            "icon-shengri",
-            "icon-canju",
-            "icon-yiliao",
-            "icon-huaban"]
-    ,
-    incomeIcon: [
-        "icon-lijin",
-        "icon-licai",
-        "icon-jianzhi",
-        "icon-yuangonggongzi"
-    ]
+  payIcon: ["icon-yundong",
+    "icon-youxi",
+    "icon-yundong1",
+    "icon-lvyou",
+    "icon-gouwu",
+    "icon-jiaotong",
+    "icon-shenghuo",
+    "icon-xuexi-",
+    "icon-shengri",
+    "icon-canju",
+    "icon-yiliao",
+    "icon-huaban"
+  ],
+  incomeIcon: [
+    "icon-lijin",
+    "icon-licai",
+    "icon-jianzhi",
+    "icon-yuangonggongzi"
+  ]
 
 }
--- src/pages/my/main.js
+++ src/pages/my/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
+import VueCookies from 'vue-cookies'
 import App from './index'
 
+Vue.use(VueCookies)
 const app = new Vue(App)
 app.$mount()
